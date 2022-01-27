a735c1767544e88205d8b4941739e27a4d641676
Gevorg <67325643+Gevorg1990@users.noreply.github.com>
2022-01-27 12:32:57 +0400

    eee

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const https = require('https');
 const WebSocket = require('ws');
 
 const port = 6969;
-const server = http.createServer('https://gevorg1990.github.io/socet/');
+const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
 wss.on('connection', function connection(ws) {
